685a7f70ba412a94c430b85349eeb3b759341957
Sheheryar Ahmad Pirzada <sheheryarpirzada@wanclouds.net>
2021-11-30 21:35:24 +0500

    adds xss filter

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.xssFilter());
 
 module.exports = app;
 const api = require('./server.js');
